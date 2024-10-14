885214838d2049513c2f0552231e01ba358d3d36
Rakesh <rakesh.vuyyuru.19@gmail.com>
2024-10-14 07:06:12 +0530

    Setting up project

---
+++ src/main/java/com/rakey/EazyShop/EazyShopApplication.java
@@ -0,0 +1,13 @@
+package com.rakey.EazyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakey/EazyShop/EazyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakey.EazyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

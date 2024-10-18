739eb1ad5249e3fd0d32e3bf4c4a805eef8965f3
Rakesh <rakesh.vuyyuru.19@gmail.com>
2024-10-18 13:01:37 +0530

    Adding CartController

---
+++ src/main/java/com/rakey/EazyShop/controller/CartController.java
@@ -0,0 +1,51 @@
+package com.rakey.EazyShop.controller;
+
+import com.rakey.EazyShop.exceptions.ResourceNotFoundException;
+import com.rakey.EazyShop.model.Cart;
+import com.rakey.EazyShop.response.ApiResponse;
+import com.rakey.EazyShop.service.cart.ICartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+
+    @Autowired
+    private ICartService cartService;
+
+    @GetMapping("/{cartId}/my-cart")
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId){
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Success",cart));
+        } catch (ResourceNotFoundException e) {
+           return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @DeleteMapping("/{cartId}/clear")
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable long cartId){
+        try {
+            cartService.clearCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Clear success",null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping("/{cartId}/cart/total-price")
+    public ResponseEntity<ApiResponse> getTotalAmount(@PathVariable Long cartId){
+        try {
+            BigDecimal totalPrice = cartService.getTotalPrice(cartId);
+            return ResponseEntity.ok(new ApiResponse("Total price",totalPrice));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+}
